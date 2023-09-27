5ede3b566d91c322833ab81e8d65ba663de9a2ef
charmingruby <mrrubycanal3105@gmail.com>
2023-09-27 01:21:48 -0300

    fixed checkbox errors

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,15 +2,30 @@ import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { TabletSmartphone } from 'lucide-react'
+import Link from 'next/link'
 
 export default function Login() {
   return (
     <div className="lg:h-screen grid grid-cols-2">
-      <section className="max-w-md mx-auto w-full">
+      <section className="max-w-md mx-auto w-full flex flex-col justify-center">
         {/* Intro */}
         <div>
           {/* Logo */}
+          <TabletSmartphone />
+
           {/* Title */}
+          <div>
+            <h1>Entrar na sua conta</h1>
+
+            <span>
+              Não possui conta?{' '}
+              <Link prefetch={false} href="/">
+                Criar uma conta
+              </Link>
+            </span>
+          </div>
+
           {/* Sign up */}
         </div>
 
