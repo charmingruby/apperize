eacf514ece245268d6b84e2217815792a46b8aa4
charmingruby <mrrubycanal3105@gmail.com>
2024-01-11 21:24:52 -0300

    chore: changed menu button color

--- src/app/(public)/_components/navigation/header/menu-button.tsx
+++ src/app/(public)/_components/navigation/header/menu-button.tsx
@@ -1,15 +1,15 @@
 'use client'
 
 import { MenuContext } from '@/contexts/menu-context'
-import { Menu, X } from 'lucide-react'
+import { Menu } from 'lucide-react'
 import { useContext } from 'react'
 
 export function MenuButton() {
   const { isOpen, toggleMenu } = useContext(MenuContext)
 
   return (
-    <button className="block lg:hidden text-primary" onClick={toggleMenu}>
-      {isOpen ? <X /> : <Menu />}
+    <button className="block lg:hidden" onClick={toggleMenu}>
+      <Menu className={isOpen ? 'invisible' : 'block'} />
     </button>
   )
 }
