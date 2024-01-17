7960f35d104ee40084d6e3488f61e34b205f8925
Felipe-Bignoto <felipebignoto@gmail.com>
2024-01-17 20:25:01 -0300

    chore: updating text and image depositions contents

--- src/contents/depositions.ts
+++ src/contents/depositions.ts
@@ -4,3 +4,31 @@ export interface DepositionItem {
   role: string
   name: string
 }
+
+export const depositionItem: DepositionItem[] = [
+  {
+    avatarUrl: '@/assets/img/depositions/foto_romulo.jpeg',
+    deposition:
+      'Estou muito realizado com os serviços da Apperize. Recebi um suporte incrível na criação do Websites Institucional da minha empresa de Consultoria. Toda a equipe é muito atenciosa, ágil e conseguiram personalizar 100% meu site!',
+    name: 'Rômulo Valle',
+    role: 'Consultoria Digital RV',
+  },
+  {
+    avatarUrl: '',
+    deposition: '',
+    name: '',
+    role: '',
+  },
+  {
+    avatarUrl: '',
+    deposition: '',
+    name: '',
+    role: '',
+  },
+  {
+    avatarUrl: '',
+    deposition: '',
+    name: '',
+    role: '',
+  },
+]
